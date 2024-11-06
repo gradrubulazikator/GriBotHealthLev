8399e8f7c6c244b5301fcab48fed688d4fa61607
gradrubulazikator <2zl4tyba67hg3@proton.me>
2024-11-07 00:24:25 +0500

    Рефакторинг кода: добавил config и utils в internal

---
+++ internal/config.go
@@ -0,0 +1,11 @@
+
+package internal
+
+import "time"
+
+const (
+	BotToken    = "7215760944:AAF3fveH7KFHOe1HVvxICFGbx27fzQGAkMU" // Убедитесь, что этот токен не виден публично
+	ChatID      = "5838863003"
+	RemindDelay = 2 * time.Hour
+)
+
--- main.go
+++ main.go
@@ -1,59 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"net/url"
-	"time"
+    "log"
+    "time"
+    "GriBotHealthLev/internal" // Импортируем пакет internal, чтобы использовать его функции и константы
 )
-
-const telegramAPI = "https://api.telegram.org/bot"
-
-var (
-	botToken    = "7215760944:AAF3fveH7KFHOe1HVvxICFGbx27fzQGAkMU"
-	chatID      = "5838863003"
-	remindDelay = 2 * time.Hour // Измените на более короткий интервал для тестирования
-)
-
-func sendMessage(message string) error {
-	endpoint := fmt.Sprintf("%s%s/sendMessage", telegramAPI, botToken)
-	data := url.Values{}
-	data.Set("chat_id", chatID)
-	data.Set("text", message)
-
-	resp, err := http.PostForm(endpoint, data)
-	if err != nil {
-		return fmt.Errorf("ошибка отправки сообщения: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("ошибка ответа от Telegram: %s", resp.Status)
-	}
-	return nil
-}
 
 func healthReminder() {
-	messages := []string{
-		"Пора сделать разминку!",
-		"Не забудьте выпить воды.",
-		"Пора проверить уровень стресса.",
-	}
+    messages := []string{
+        "Пора сделать разминку!",
+        "Не забудьте выпить воды.",
+        "Пора проверить уровень стресса.",
+    }
 
-	for {
-		for _, msg := range messages {
-			if err := sendMessage(msg); err != nil {
-				log.Printf("Ошибка отправки напоминания: %v", err)
-			}
-			time.Sleep(remindDelay)
-		}
-	}
+    for {
+        for _, msg := range messages {
+            if err := internal.SendMessage(msg); err != nil { // Используем internal.SendMessage
+                log.Printf("Ошибка отправки напоминания: %v", err)
+            }
+            time.Sleep(internal.RemindDelay) // Используем internal.RemindDelay
+        }
+    }
 }
 
 func main() {
-	log.Println("Запуск GriBotHealthLev...")
-	go healthReminder()
-	select {}
+    log.Println("Запуск GriBotHealthLev...")
+    go healthReminder()
+    select {}
 }
 
